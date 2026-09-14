8c77b476d5ecbe0c74e4fc1477892c6dfc1ad313
Your Name <your_email@example.com>
2026-09-14 21:57:55 +0300

    1234567

--- app/src/main/java/kernel/unisocsu/easysu/MainActivity.java
+++ app/src/main/java/kernel/unisocsu/easysu/MainActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import java.io.IOException;
 
 public class MainActivity extends Activity {
 	private Button checkButton;
@@ -36,4 +37,5 @@ private void checkDevice () {
 	String chipinfo = Build.HARDWARE;
 	String androidVersion = Build.VERSION.RELEASE;
 	int apiVersion = Build.VERSION.SDK_INT;
-	
+	try {
+		Process process = Runtime.getRuntime().exec("getprop");
